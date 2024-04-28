0d1754642d3f0ac7a9de42249f1ed54811e10f50
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2024-04-28 19:16:22 -0400

    lualine config

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,51 @@
+local git_blame = require('gitblame')
+-- Disable blame text next to cursor
+vim.g.gitblame_display_virtual_text = 0
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'material',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'location'},
+    lualine_c = { { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available } },
+    lualine_x = {'fileformat'},
+    lualine_y = {'filetype'},
+    lualine_z = {'hostname'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+tabline = {
+  lualine_a = {'buffers'},
+  lualine_b = {},
+  lualine_c = {},
+  lualine_x = {},
+  lualine_y = {'diff', 'diagnostics'},
+  lualine_z = {'branch'}
+},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
--- lua/monoald/packer.lua
+++ lua/monoald/packer.lua
@@ -8,44 +8,51 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    -- or                            , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use({
-	  'wadackel/vim-dogrun',
-	  as = 'dogrun',
-	  config = function()
-		  vim.cmd('colorscheme vim-dogrun')
-	  end
+    'wadackel/vim-dogrun',
+    as = 'dogrun',
+    config = function()
+      vim.cmd('colorscheme dogrun')
+    end
   })
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('theprimeagen/harpoon')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
-  use {
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		  -- {'williamboman/mason.nvim'},
-		  -- {'williamboman/mason-lspconfig.nvim'},
+  use({
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+      --- Uncomment the two plugins below if you want to manage the language servers from neovim
+      -- {'williamboman/mason.nvim'},
+      -- {'williamboman/mason-lspconfig.nvim'},
 
-		  {'neovim/nvim-lspconfig'},
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-	  }
-  }
+      {'neovim/nvim-lspconfig'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+    }
+  })
 
 
-use {
+  use ({
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
-}
+  })
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
+  use('f-person/git-blame.nvim')
 
 end)
 
